b1b9dab49bf20a93bc7afe140d6404084d6c7fe4
mintyrain882 <52992084+mintyrain882@users.noreply.github.com>
2019-10-24 15:05:55 +0800

    ...

--- t01primeshcflcm_findab1.py
+++ t01primeshcflcm_findab1.py
@@ -1,2 +1,5 @@
-# If a and b are integers such that a * b = 17, find the value of a + b.
+# If a and b are integers such that a * b = 17, find the value of a + b. 
+
+
+# 17 can only be divided by 1 and 17 as it is a prime number
 
